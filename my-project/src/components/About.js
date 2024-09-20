e0ad5f148685f296d2308dc369cca711aa2a0002
shraddhaM24 <shraddhamistry37@gmail.com>
2024-09-20 13:28:17 +0530

    class base component - ComponentDidMount

--- my-project/src/components/About.js
+++ my-project/src/components/About.js
@@ -1,14 +1,55 @@
+import { Component } from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () =>{
-    return (
-        <div>
-            <p>About US</p>
-            <User name={"Shraddha Mistry (function)"} />
-            <UserClass name={"Shraddha Mistry (Class)"} location={"Mumbai (Class))"} />
-        </div>
-    )
-};
+class About extends Component{
+
+    constructor(props) {
+        super(props)
+        console.log("Parent Constructor");
+    }
+
+    componentDidMount(){
+        console.log("Parent Mount");
+    }
+
+    render() {
+        console.log("Parent render");
+        return (
+            <div>
+                <p>About US</p>
+                {/* <User name={"Shraddha Mistry (function)"} /><br/><hr/> */}
+                <UserClass name={"First"} location={"Mumbai (Class))"} />
+                <UserClass name={"Second"} location={"Mumbai (Class))"} />
+                <UserClass name={"Third"} location={"Mumbai (Class))"} />
+            </div>
+        )
+    }
+}
+
+// const About = () =>{
+//     return (
+//         <div>
+//             <p>About US</p>
+//             <User name={"Shraddha Mistry (function)"} /><br/><hr/>
+//             <UserClass name={"Shraddha Mistry (Class)"} location={"Mumbai (Class))"} />
+//         </div>
+//     )
+// };
+
+/*
+    - Parent Constructor
+    - Parent render
+        - First constructor
+        - First render
+
+        - Second constructor
+        - Second render
+
+        <DOM update - in single batch>
+        - First Mount
+        - Second Mount
+    - Parent Mount
+*/
 
 export default About;
--- my-project/src/components/UserClass.js
+++ my-project/src/components/UserClass.js
@@ -4,15 +4,40 @@ class UserClass extends React.Component{
 
     constructor(props){
         super(props);
+
+        this.state = {
+            count:0,
+            count2:2,
+        }
+
+        console.log(this.props.name+" constructor");
+    }
+
+    componentDidMount(){
+        console.log(this.props.name+" Mount");
     }
 
     render(){
+        console.log(this.props.name+" render");
         const {location} = this.props;
+        const {count,count2} = this.state;
         return(
             <div className="user-card">
+                {/* <h3>Count: {count}</h3>
+                <button onClick={() => {
+                    this.setState({
+                        count: this.state.count + 1,
+                    })
+                }}>Count increase</button>
+                <button onClick={() => {
+                    this.setState({
+                        count: this.state.count - 1,
+                    })
+                }}>Count decrease</button>
+                <h3>Count2: {count2}</h3>
                 <h2>Name : {this.props.name}</h2>
                 <h3>Location : {location}</h3>
-                <h4>Contact : shraddhamistry37@gmail.com</h4>
+                <h4>Contact : shraddhamistry37@gmail.com</h4> */}
             </div>
         )
     }
